322dd320a1f1db331f1176b7555a6e0d28bd9734
josuegradiz <jdgl200423@gmail.com>
2021-11-21 00:55:00 -0600

    modificando el codigo con vs code

--- Intrefaz_Gráfica_Grupo6/src/intrefaz_gráfica_grupo6/Intrefaz_Gráfica_Grupo6.java
+++ Intrefaz_Gráfica_Grupo6/src/intrefaz_gráfica_grupo6/Intrefaz_Gráfica_Grupo6.java
@@ -14,7 +14,7 @@ public class Intrefaz_Gráfica_Grupo6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("intentando usar java en vs code");
     }
     
 }
