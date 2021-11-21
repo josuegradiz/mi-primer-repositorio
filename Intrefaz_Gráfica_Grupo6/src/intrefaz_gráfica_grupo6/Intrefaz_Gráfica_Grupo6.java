fa1e079b80351c679598fd2db28506b2dccac09b
josuegradiz <jdgl200423@gmail.com>
2021-11-20 21:34:26 -0600

    agregando el archivo de programa java

---
+++ Intrefaz_Gráfica_Grupo6/src/intrefaz_gráfica_grupo6/Intrefaz_Gráfica_Grupo6.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package intrefaz_gráfica_grupo6;
+
+/**
+ *
+ * @author jdgl2
+ */
+public class Intrefaz_Gráfica_Grupo6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Intrefaz_Gráfica_Grupo6/src/intrefaz_gráfica_grupo6/interfaz.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package intrefaz_gráfica_grupo6;
+
+/**
+ *
+ * @author jdgl2
+ */
+public class interfaz {
+    
+}
